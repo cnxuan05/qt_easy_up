bf1a256a797d916d94ba9e060696b96a4841544f
cnxuan05 <cnxuan05@gmail.com>
2016-04-06 21:48:00 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,51 @@
+
+#include <QApplication>
+
+#include <QWidget>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QCompleter>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+
+int main(int argc,char *argv[])
+{
+
+    QApplication app(argc,argv);
+
+    QWidget w;
+
+    QLineEdit edit;
+    //edit.show();
+    edit.setParent(&w);
+
+    QCompleter completer(QStringList() << "abc" << "123" << "qwe");
+    completer.setFilterMode(Qt::MatchContains);
+    edit.setCompleter(&completer);
+
+
+    w.show();
+
+
+
+    QPushButton button;
+    button.setText("Button");
+
+    button.setParent(&w);
+    button.show();
+
+    button.setGeometry(90,90,200,200);
+
+    QHBoxLayout layout;
+    layout.addStretch(1);
+    layout.addWidget(&edit);
+    layout.addWidget(&button);
+    layout.addStretch(1);
+    w.setLayout(&layout);
+
+    QObject::connect(&button,SIGNAL(clicked()),&w,SLOT(close()));
+
+    w.setWindowTitle("hello world");
+
+    return app.exec();
+}
